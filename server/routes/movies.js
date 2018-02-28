f38d2f88b0435ca4632ef61e59e375f4a9e508f6
Arthur de Carné <arthurdc09@gmail.com>
2018-02-28 14:11:51 +0100

    m

---
+++ server/routes/movies.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var router = express.Router();
+var apiKey = f04b2a25baed952b84af0eb4623bbc55;
+
+/* Get Movie by Keywords */
+
+router.get("/mood/:id", function(req, res, next) {
+  router.get(mood._id, function(req, res, next) {});
+  res.json();
+});
+
+module.exports = router;
