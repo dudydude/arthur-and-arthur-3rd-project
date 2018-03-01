63f91c98269cd5eb4582fcc3643acabe1d27a55a
Arthur M <arthur.mossa@gmail.com>
2018-03-01 22:07:15 +0100

    Cookie M&S v2.2

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,5 +1,19 @@
 <template>
 <div class="text-center">
-<h1>Food & Mood - coz Netflix'nd Chill is out of date.</h1>
+<h1 class="m-2 mt-3">Food & Mood - coz Netflix'nd Chill is out of date.</h1>
+<hr class="m-3">
+<h3 class="m-2">The Concept</h3>
+<p class="text-justify mx-5 my-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam facilisis et est ut rutrum. 
+Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+Vivamus semper finibus scelerisque. Nunc convallis nulla eget diam egestas, vitae pharetra dolor suscipit. 
+Donec malesuada congue urna et pretium. Integer dignissim tortor eu finibus venenatis. 
+Nulla vel ipsum sed felis faucibus tempus sed eu mi.</p>
+
+<h3 class="m-2 mt-5">How to use the app</h3>
+<p class="text-justify mx-5 my-2">Nulla at libero scelerisque, placerat leo eget, mattis arcu. Mauris quis dui et tellus sagittis bibendum vel eu lorem. 
+    Integer eget aliquam odio. In aliquet vehicula condimentum. Sed sodales leo sit amet diam rutrum, ac ultrices nisl fermentum. 
+    Pellentesque at ultrices tellus. Suspendisse sed sodales nisi. Suspendisse sagittis fringilla quam, non suscipit lectus tincidunt suscipit. Nam quam mauris, placerat et odio vitae, faucibus congue est. Integer vestibulum lectus sit amet imperdiet molestie. Fusce nec posuere odio, et venenatis risus. Integer egestas nibh ac leo dignissim vehicula. Pellentesque nibh dolor, feugiat at faucibus non, consequat vitae massa. 
+    Sed vestibulum vitae erat at maximus. Phasellus cursus orci sed dapibus blandit. Phasellus et risus dolor.</p>
+<hr class="m-3">
 </div>
 </template>
