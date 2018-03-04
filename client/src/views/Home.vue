1e0f7f965d876d6e522da8b63e30cfa2f14e8922
Arthur M <arthur.mossa@gmail.com>
2018-03-04 21:18:17 +0100

    Speculos 1.1

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
-<div class="text-center container">
-<h1 class="m-2 mt-3">Food & Mood - coz Netflix'nd Chill is out of date.</h1>
-<hr class="m-3">
-<h3 class="m-2">The Concept</h3>
-<p class="text-justify mx-5 my-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam facilisis et est ut rutrum. 
-Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
-Vivamus semper finibus scelerisque. Nunc convallis nulla eget diam egestas, vitae pharetra dolor suscipit. 
-Donec malesuada congue urna et pretium. Integer dignissim tortor eu finibus venenatis. 
-Nulla vel ipsum sed felis faucibus tempus sed eu mi.</p>
-<br>
-<router-link :to="`/profile`" class="button-profile"> Go </router-link>
-<br>
-</div>
+  <div class="text-center container">
+    <h1 class="m-2 mt-3">Food & Mood - coz Netflix'nd Chill is out of date.</h1>
+    <hr class="m-3">
+    <div>
+      <h3 class="m-2">The Concept</h3>
+      <p class="text-justify mx-5 my-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam facilisis et est ut rutrum. Class aptent taciti sociosqu
+        ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus semper finibus scelerisque. Nunc convallis
+        nulla eget diam egestas, vitae pharetra dolor suscipit. Donec malesuada congue urna et pretium. Integer dignissim
+        tortor eu finibus venenatis. Nulla vel ipsum sed felis faucibus tempus sed eu mi.</p>
+      <br>
+      <router-link :to="`/profile`" class="button-profile m-3 btn">GO✨✨🚀</router-link>
+      <br>
+      <br>
+      <hr class="m-3">
+    </div>
+  </div>
 </template>
--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -1,5 +1,5 @@
 <template>
-<<<<<<< HEAD
+
   <section class="text-center container">
     <div>
       <h2>Salut à toi {{$root.user.username}}</h2>
@@ -27,26 +27,7 @@
 
       <router-link class="button-profile" to="/mood">Movies that match my mood</router-link>
     </div>
-
-    </div>
-
   </section>
-=======
-<section class="text-center">
-<div>
-<h2>Salut à toi {{$root.user.username}}</h2>
-</div>
-<div>
-  <b-button variant="primary" class="m-3 btn-lg" to="/food">In mood for some food</b-button>
-</div>
-<div>
-  <b-button variant="warning" class="m-3 btn-lg" to="/mood">Movies that match my mood</b-button>
-</div>
-<div class="m-5 px-5">
-<hr>
-</div>
-   </section>
->>>>>>> d7debd62e1920a9d2982d59518772631d7919f05
 </template>
 
 <script>
