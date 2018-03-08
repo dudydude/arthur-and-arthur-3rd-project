57f84bd887efd532923d3e8f5e47aeb6cdfd60ce
Arthur M <arthur.mossa@gmail.com>
2018-03-08 15:22:06 +0100

    DCM food part done

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -22,14 +22,51 @@ router.get("/searchAll", function(req, res, next) {
 // find recipes based on ingredients
 router.get("/searchbyingredients/:ingredient", (req, res, next) => {
   let orResearch = [];
-  let ingredientSplit = req.params.ingredient.split(" ");
+  let ingredientSplit = req.params.ingredient.split("+");
+  const getScore = function(userIngredients, recipe) {
+    let researchScore = 0;
+    const iterate = function(arr) {
+      for (let i = 0; i < arr.length; i++) {}
+    };
+    // console.log(ingredients.map(i => i.ingredients));
+
+    for (
+      let iUserIngredient = 0;
+      iUserIngredient < userIngredients.length;
+      iUserIngredient++
+    ) {
+      for (
+        let iRecipeIngredient = 0;
+        iRecipeIngredient < recipe.ingredients.length;
+        iRecipeIngredient++
+      ) {
+        if (
+          recipe.ingredients &&
+          recipe.ingredients[iRecipeIngredient] &&
+          recipe.ingredients[iRecipeIngredient].match(
+            userIngredients[iUserIngredient].ingredients
+          )
+        ) {
+          researchScore++;
+        }
+      }
+    }
+    return researchScore;
+  };
+  // };
   ingredientSplit.forEach(ingredient => {
     const regex = new RegExp(ingredient, "i");
     orResearch.push({ ingredients: regex });
   });
+
+  console.log("orResearch: ", orResearch);
   Recipe.find({ $or: orResearch })
-    .then(recipe => {
-      res.json(recipe);
+    .then(recipes => {
+      for (let i = 0; i < recipes.length; i++) {
+        recipes[i].score = getScore(orResearch, recipes[i]);
+      }
+      recipes.sort((a, b) => b.score - a.score);
+      res.json(recipes);
     })
     .catch(err => next(err));
 });
